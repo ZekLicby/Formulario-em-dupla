756a6f8f68eaa400563e766612317375f059916f
Ramon Silva <ramon.silva109@gmail.com>
2022-10-21 11:31:46 -0300

    adding a config to connect in my db

---
+++ inc/config.php
@@ -0,0 +1,31 @@
+<?php 
+
+define('MYSQL_OPTIONS',[
+    'db_host' => 'localhost',
+    'db_name' => '',
+    'db_user' => '',
+    'db_pass' => '',
+    'db_char' => 'utf8',
+
+]);
+
+define('AES_KEY', 'YmvF7r6p7ifBW9wRvtlBUeYZTkt83xYC');
+define('AES_IV', 'qll4I1mzpcw1PTaL');
+
+// to encrypt
+function aes_encrypt($valor)
+{
+    return bin2hex(openssl_encrypt($valor, 'aes-256-cbc', AES_KEY,OPENSSL_RAW_DATA,AES_IV));
+}
+
+// to decrypt
+/* function aes_decrypt($hash)
+{
+    //verificar se a harsh é valida 
+    if(strlen($hash) % 2 != 0){
+        return -1;
+    }
+    
+    return openssl_decrypt(hex2bin($hash),'aes-256-cbc', AES_KEY,OPENSSL_RAW_DATA,AES_IV);
+}
+ */
--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+require_once('inc/config.php');
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
